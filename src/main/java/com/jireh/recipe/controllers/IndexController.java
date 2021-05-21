69ae245ef26e1d10b1a94d5bef87e473353f6fa5
Jireh-IT <jireh.kapanga@dkkcorp.com>
2021-05-21 22:15:23 +0100

    Query method JPA

--- src/main/java/com/jireh/recipe/controllers/IndexController.java
+++ src/main/java/com/jireh/recipe/controllers/IndexController.java
@@ -1,7 +1,32 @@
 package com.jireh.recipe.controllers;
 
+import com.jireh.recipe.model.Category;
+import com.jireh.recipe.model.UnitOfMeasure;
+import com.jireh.recipe.repositories.CategoryRepository;
+import com.jireh.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"","/","index"})
+    public String getIndexPage(){
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("African");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByUom("TeaSpoon");
+
+        System.out.println("Cat Id is: "+optionalCategory.get().getId());
+        System.out.println("UOM Id is: "+optionalUnitOfMeasure.get().getId());
+        return "index";
+    }
 }
--- src/main/java/com/jireh/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/jireh/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.jireh.recipe.repositories;
 import com.jireh.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+
+    Optional<UnitOfMeasure> findByUom(String uom);
 }
